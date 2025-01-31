d40a7cfb49cb2727ff41c23ff7e00c75fdbd726d
cesargm1 <cg.gtav@gmail.com>
2025-01-31 15:15:22 +0100

    first commit

---
+++ src/components/navbarMenu.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="nav">
+        <ul class="nav__list">
+
+            <li class="nav__item">
+                <a class="nav__item__link" href="#enlace1">enlace1</a>
+            </li>
+
+            <li class="nav__item">
+                <a class="nav__item__link" href="#enlace2">enlace2</a>
+            </li>
+
+            <li class="nav__item">
+                <a class="nav__item__link  nav__item__link--active" href="#enlace3">enlace3</a>
+            </li>
+
+            <li class="nav__item">
+                <a class="nav__item__link  nav__item__link--hover" href="#enlace4">enlace4</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script setup>
+</script>
+
+<style scoped></style>
